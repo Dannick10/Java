b4faadc14519264d9a0c4d8c56fa2dc8ede8b0fa
Dannick010 <danmaggot2@gmail.com>
2025-05-29 18:47:39 -0300

    feat/casting

--- src/academy/devdojo/maratona/introducao/aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratona/introducao/aula02TiposPrimitivos.java
@@ -3,7 +3,20 @@ package academy.devdojo.maratona.introducao;
 public class aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
+        // (int) casting
+        int age = (int) 10L;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0;
+        byte idadeByte = 10;
+        short idadeSHort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
         System.out.println("A idade é "+age+" anos");
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+
     }
 }
