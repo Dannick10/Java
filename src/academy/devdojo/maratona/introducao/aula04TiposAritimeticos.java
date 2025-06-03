2a4bc6037158a64d2b7a646bf0e7dcea147ab582
Dannick010 <danmaggot2@gmail.com>
2025-06-03 16:49:12 -0300

    operadores aritimeticos

---
+++ src/academy/devdojo/maratona/introducao/aula04TiposAritimeticos.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratona.introducao;
+
+public class aula04TiposAritimeticos {
+
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+    }
+}
