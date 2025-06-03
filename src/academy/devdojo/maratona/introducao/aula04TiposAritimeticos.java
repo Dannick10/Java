2ed7d9a34c2e7b9c7459c0964c65da3a04d4f9bd
Dannick010 <danmaggot2@gmail.com>
2025-06-03 17:04:27 -0300

    operadores relacionais

--- src/academy/devdojo/maratona/introducao/aula04TiposAritimeticos.java
+++ src/academy/devdojo/maratona/introducao/aula04TiposAritimeticos.java
@@ -7,5 +7,23 @@ public class aula04TiposAritimeticos {
         int numero02 = 20;
         double resultado = numero01 / numero02;
         System.out.println(resultado);
+
+        //  %
+        int resto = 21% 6;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 20;
+
+        System.out.println("maior que vinte "+isDezMaiorQueVinte);
+        System.out.println("menor que vinte "+isDezMenorQueVinte);
+        System.out.println("igual a dez "+isDezIgualDez);
+        System.out.println("igual a vinte "+isDezIgualVinte);
+        System.out.println("diferente de dez "+isDezDiferenteDez);
+
     }
 }
